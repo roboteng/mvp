8c351d8476ba9925054640d9c7bf4325ba8dbca4
Trevor Settles <roboteng@gmail.com>
2021-10-12 11:34:03 -0600

    added New Note button

--- src/routes/ViewAll/ViewAll.tsx
+++ src/routes/ViewAll/ViewAll.tsx
@@ -1,7 +1,12 @@
 
 function ViewAll() {
   return <>
-    <h1>All Notes</h1>
+    <div className="header">
+      <h1>All Notes</h1>
+    </div>
+    <button className="new-note">
+      New Note
+    </button>
   </>
 }
 

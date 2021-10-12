bdaf0cca5f82285c4f9bb74dba42eb0cf4f62f5d
Trevor Settles <roboteng@gmail.com>
2021-10-12 15:56:51 -0600

    sent post to the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import api from './api';
 import './App.css';
 import Note from './models/Note';
 import NewNote from './routes/NewNote/NewNote';
 import ViewAll from './routes/ViewAll/ViewAll';
 
 const App = () => {
-  const saveNote = (note: Note) => console.log(note);
+  const saveNote = (note: Note) => {
+    api.saveNote(note);
+  }
   return (
     <Router >
       <Route exact={true} path="/" component={ViewAll} />

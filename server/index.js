54d183856bcf6b2cba233a694ed8e880ec8f6991
Trevor Settles <roboteng@gmail.com>
2021-10-12 12:03:54 -0600

    served index.html on server

--- server/index.js
+++ server/index.js
@@ -5,7 +5,15 @@ const port = 3001;
 
 const app = express();
 
-app.use(express.static(path.join('..', 'public')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
+
+app.use((req, res, next) => {
+  console.log(`Got ${req.method} on ${req.url}`);
+})
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, ()=> {
   console.log(`Listening on http://localhost:${port}`)

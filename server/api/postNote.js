c406697b557fe84c3b0e96fb5f03cab582527bbf
Trevor Settles <roboteng@gmail.com>
2021-10-12 16:30:09 -0600

    adde basic error handling

--- server/api/postNote.js
+++ server/api/postNote.js
@@ -1,11 +1,21 @@
 const con = require('../db/db.js');
 
 const postNote = (req, res, next) => {
-  con.query(`
-  INSERT INTO note (title, content)
-  VALUES ('${req.body.title}', '${req.body.content}');`)
-  res.status(201).send();
-  next();
+  if (req.body?.title) {
+    con.query(`
+    INSERT INTO note (title, content)
+    VALUES ('${req.body.title}', '${req.body.content}');`, (err, results) => {
+      if (err) {
+        res.status(500).send();
+      } else {
+        res.status(201).send();
+      }
+      next();
+    })
+  } else {
+    res.status(400).send("title must be provided");
+    next();
+  }
 };
 
 module.exports = postNote;

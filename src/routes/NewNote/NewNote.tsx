834fb656a4d2abca3e82303eaf8d8da5a7c00f99
Trevor Settles <roboteng@gmail.com>
2021-10-12 14:39:38 -0600

    added new note form

--- src/routes/NewNote/NewNote.tsx
+++ src/routes/NewNote/NewNote.tsx
@@ -1,9 +1,25 @@
+import './NewNote.css';
+
 export default function NewNote() {
   return (
     <>
-    <div className="header">
-      <h1>New Note</h1>
-    </div>
+      <div className="header">
+        <h1>New Note</h1>
+      </div>
+      <main>
+        <form>
+          <label className="title field">
+            <p>Title:</p>
+            <input type="text"
+            ></input>
+          </label>
+          <label className="contents field">
+            <p>Contents:</p>
+            <textarea
+            ></textarea>
+          </label>
+        </form>
+      </main>
     </>
   );
 }

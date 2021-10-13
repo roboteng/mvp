6575fa8ce25eab27d202951086a1ac10a8392e58
Trevor Settles <roboteng@gmail.com>
2021-10-12 19:43:46 -0600

    added delete button

--- src/routes/ViewAll/NoteTile.tsx
+++ src/routes/ViewAll/NoteTile.tsx
@@ -1,14 +1,24 @@
+import './NoteTile.css';
 import SavedNote from "../../models/SavedNote";
+import { useState } from 'react';
 
 interface NoteTileProps {
   note: SavedNote,
 }
 
 function NoteTile(props: NoteTileProps) {
+  const [showDelete, changeShowDelete] = useState(false);
   return (
-    <div id={props.note.id.toString()}>
-      <p>{props.note.title}</p>
-      <p>{props.note.content}</p>
+    <div id={props.note.id.toString()}
+      className="tile"
+      onMouseEnter={() => changeShowDelete(true)}
+      onMouseLeave={() => changeShowDelete(false)}
+    >
+      <div>
+        <p>{props.note.title}</p>
+        <p>{props.note.content}</p>
+      </div>
+      {showDelete ? <button>Delete</button> : null}
     </div>
   )
 }
